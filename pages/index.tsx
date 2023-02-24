bd69e47ec36aa0ce06b8ec25005cfe97bb090be5
Moiz-I <sky.moizi9@gmail.com>
2023-02-24 21:46:49 +0000

    removed nextspinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import memoji from "../public/memoji.png";
-import { useState, CSSProperties } from "react";
-import { NextProgressbarSpinner } from "nextjs-progressbar-spinner";
 
 import Home from "./home";
 
 export default function Index() {
     return (
         <>
-            <NextProgressbarSpinner />
             <Home />
         </>
     );
