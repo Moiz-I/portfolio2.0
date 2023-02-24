d4310d67399aaaca027d08f2887f59bcc58828c7
Moiz-I <sky.moizi9@gmail.com>
2023-02-24 23:04:20 +0000

    added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import memoji from "../public/memoji.png";
+import Head from "next/head";
+import favicon from "../public/grad1.svg";
 
 import Home from "./home";
 
 export default function Index() {
     return (
         <>
+            <Head>
+                <title>Moiz Imran</title>
+                <link rel="shortcut icon" href="/grad1.svg" sizes="32x32" />
+            </Head>
             <Home />
         </>
     );
