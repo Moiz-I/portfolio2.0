67b5c8fbf7ebeef8a536d968a9bc9d4e54983563
Moiz-I <sky.moizi9@gmail.com>
2024-04-19 17:57:26 +0100

    portfolio rehaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
-    theme: {
-        extend: {},
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "instrument-serif": ["Instrument Serif", "serif"],
+      },
+      backgroundImage: (theme) => ({
+        squiggle: "url('https://snippets.alexandru.so/squiggle.svg')",
+      }),
     },
-    plugins: [],
+  },
+  plugins: [],
 };
